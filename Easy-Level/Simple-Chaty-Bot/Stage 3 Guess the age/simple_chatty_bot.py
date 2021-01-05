6904bd78e1e23e65f11aa4b46861b4ada3f5c37a
Qaminono <qaminono@gmail.com>
2021-01-05 15:18:44 +0300

    Project add

---
+++ Easy-Level/Simple-Chaty-Bot/Stage 3 Guess the age/simple_chatty_bot.py
@@ -0,0 +1,14 @@
+print('Hello! My name is Aid.')
+print('I was created in 2020.')
+print('Please, remind me your name.')
+
+name = input()
+
+print('What a great name you have, ' + name + '!')
+print('Let me guess your age.')
+print('Enter remainders of dividing your age by 3, 5 and 7.')
+
+remainder3, remainder5, remainder7 = int(input()), int(input()), int(input())
+your_age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+
+print(f"Your age is {your_age}; that's a good time to start programming!")
