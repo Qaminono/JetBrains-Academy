f898fbfb16eb63a00ef9b09322e015862c1f004f
Qaminono <qaminono@gmail.com>
2021-01-15 00:22:26 +0200

    Project add

--- Medium-Level/To-Do List/Stage 3 Deadlines are scary!/to_do_list.py
+++ Medium-Level/To-Do List/Stage 3 Deadlines are scary!/to_do_list.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Date
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 from sqlalchemy.orm import sessionmaker
 
 
