20f94df512629c402bf0a8e66834245dd1c587e7
Qaminono <qaminono@gmail.com>
2021-01-20 00:18:28 +0200

    Project add

---
+++ Hard-Level/Multilingual Online Translator/Stage 1 Repeater/translator.py
@@ -0,0 +1,4 @@
+language_choice = input('Type "en" if you want to translate from French into English, '
+                        'or "fr" if you want to translate from English into French:')
+word_choice = input("Type the word you want to translate:")
+print(f'You chose "{language_choice}" as the language to translate "{word_choice}" to.')
