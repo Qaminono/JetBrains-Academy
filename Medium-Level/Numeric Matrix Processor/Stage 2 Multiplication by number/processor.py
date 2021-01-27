35eb1ae9172c42bfe9a310e04a4edba3af5cd4b5
Qaminono <qaminono@gmail.com>
2021-01-27 15:49:15 +0200

    Revert "Project add"
    This reverts commit 7d5bb3fb

--- Medium-Level/Numeric Matrix Processor/Stage 2 Multiplication by number/processor.py
+++
@@ -1,7 +0,0 @@
-dimensions_a = [int(num) for num in input().split(" ")]
-a = [[int(num) for num in input().split(" ")] for _ in range(dimensions_a[0])]
-multiplier = int(input())
-for n in range(dimensions_a[0]):
-    for m in range(dimensions_a[1]):
-        a[n][m] = str(a[n][m] * multiplier)
-print("\n".join([" ".join(row) for row in a]))
