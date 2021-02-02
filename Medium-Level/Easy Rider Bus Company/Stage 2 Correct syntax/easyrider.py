59cc3d543a47b7397ea2bc58905f75fb0fc7ca29
Qaminono <qaminono@gmail.com>
2021-02-03 01:21:12 +0200

    refactor regexp

--- Medium-Level/Easy Rider Bus Company/Stage 2 Correct syntax/easyrider.py
+++ Medium-Level/Easy Rider Bus Company/Stage 2 Correct syntax/easyrider.py
@@ -3,18 +3,13 @@ import re
 
 
 easy_bd = json.loads(input())
-errors = {"stop_name": [0, (r"[A-Z].* Road\Z", r"[A-Z].* Avenue\Z", r"[A-Z].* Boulevard\Z", r"[A-Z].* Street\Z")],
-          "stop_type": [0, (r"F?\Z", r"O?\Z", r"S?\Z")],
-          "a_time": [0, (r"[0-1][0-9]:[0-5][0-9]\Z", r"2[0-3]:[0-5][0-9]\Z")]}
+errors = {"stop_name": [0, r"\A[A-Z].*[a-z] (Road|Avenue|Boulevard|Street)\Z"],
+          "stop_type": [0, r"\A[FOS]?\Z"],
+          "a_time": [0, r"\A([0-1][0-9]|2[0-3]):[0-5][0-9]\Z"]}
 for data in easy_bd:
     for field in errors:
-        match = False
-        for template in errors[field][1]:
-            if re.match(template, data[field]):
-                match = True
-        if not match:
+        if not re.match(errors[field][1], data[field]):
             errors[field][0] += 1
-
 
 print(f'Type and required field validation: {sum(value[0] for value in errors.values())} errors')
 for field in errors:
