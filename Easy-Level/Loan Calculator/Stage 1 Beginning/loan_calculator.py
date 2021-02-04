c2c82d90b8562ceca5e11d8890fbd6661bbe324d
Qaminono <qaminono@gmail.com>
2021-02-04 06:09:48 +0200

    add Project

--- Easy-Level/Loan Calculator/Stage 1 Beginning/loan_calculator.py
+++ Easy-Level/Loan Calculator/Stage 1 Beginning/loan_calculator.py
@@ -3,6 +3,7 @@ final_output = 'The loan has been repaid!'
 first_month = 'Month 1: repaid 250'
 second_month = 'Month 2: repaid 250'
 third_month = 'Month 3: repaid 500'
+
 print(loan_principal)
 print(first_month)
 print(second_month)
