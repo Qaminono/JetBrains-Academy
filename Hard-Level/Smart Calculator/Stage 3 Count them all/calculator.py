06076afd5b8b75b00637aaa4807a31f1d9e4f585
Qaminono <qaminono@gmail.com>
2021-02-11 16:15:30 +0200

    add Project

--- Hard-Level/Smart Calculator/Stage 3 Count them all/calculator.py
+++ Hard-Level/Smart Calculator/Stage 3 Count them all/calculator.py
@@ -2,9 +2,9 @@ while True:
     a = b = 0
     input_line = input()
     if input_line.startswith("/"):
-        comands = {"/help": "The program calculates the sum of numbers",
+        commands = {"/help": "The program calculates the sum of numbers",
                    "/exit": "Bye!"}
-        print(comands[input_line])
+        print(commands[input_line])
         if input_line == "/exit":
             break
     elif input_line:
