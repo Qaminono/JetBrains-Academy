59896cbdf8149f3a8474d65648a6d2cf25d368fc
Qaminono <qaminono@gmail.com>
2021-03-27 00:35:14 +0200

    add Project

--- Challenging-Level/HyperNews Portal/Stage 5 News search/hypernews/urls.py
+++ Challenging-Level/HyperNews Portal/Stage 5 News search/hypernews/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path
 from news.views import AllNewsView, NewsView, MainView, CreateNewsView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", MainView.as_view()),
@@ -23,3 +25,5 @@ urlpatterns = [
     path("news/create/", CreateNewsView.as_view()),
     path("news/<int:news_id>/", NewsView.as_view()),
 ]
+
+urlpatterns += static(settings.STATIC_URL)
